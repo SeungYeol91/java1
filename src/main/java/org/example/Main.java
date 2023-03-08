4ad9c83ad3f218ee335de881bea85da4f76a4032
SeungYeol91 <irenesa1126@gmail.com>
2023-03-08 10:18:40 +0900

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+    git config --global init.defaultBranch public static void main(String[]args){
+
+        }
+

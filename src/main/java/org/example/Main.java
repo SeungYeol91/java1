c4d902434450539ac1256d67f5db351cff33b8a9
SeungYeol91 <irenesa1126@gmail.com>
2023-03-13 13:22:55 +0900

    gg

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello 1231231");
+
+        System.out.println("0000000000000000000000");
     }
 }
 
